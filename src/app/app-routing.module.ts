79629715b494e12a25d0582f275ee7beb92f51aa
fareh <fareh.romero@avvale.com>
2024-05-10 19:34:40 +0200

    git start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,54 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomePageComponent,
+  // },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule) ,
+  },
+  {
+    //*cuando van a cualquier otra ruta , se les redirige  a home
+    //? Se pone el comodin al final ya que comprueva las rutas en orden
+    path: '**',
+    redirectTo: 'countries',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    //* cuando es el router principal se pone un .forRoot(modelo de rutas)
+    //?.forChild() se usa para submodulos
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    //* se exporta el rputer confirado arriba
+    RouterModule,
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent,
+  ],
+  imports: [
+    CommonModule,
+    //importamos router para poder usarlo en todo shared
+    RouterModule
+  ],
+  //se exporta para poderlo usar fuera de la carpeta shared
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent,
+  ]
+})
+export class SharedModule { }
