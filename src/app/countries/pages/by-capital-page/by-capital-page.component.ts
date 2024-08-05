4ab75dc776394b38f935018954cd5f15ccb81238
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2024-08-05 18:37:23 +0200

    s10

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -12,17 +12,20 @@ export class ByCapitalPageComponent {
 //* creamos contenedor para la respuesta  de nuestra peticion
 //? countries va a ser de tipo Country (viene el modelo de interfaces) que inicializamos con un array vacio (= [])
   public countries: Country[] = []
-
-
+  public isLoading: boolean = false;
 
   constructor(private countriesService : CountriesService){}
 
 
   searchByCapital(term : string): void {
+    this.isLoading = true;
+
 //?  para hacer la peticion hay que poner el .subscribe()
     this.countriesService.searchCapital(term).subscribe( countriesData => {
       this.countries = countriesData
-  //* los datos que regrela la llamada a la api , la guardamos en el contenedor creado arriba 
+      this.isLoading = false;
+
+  //* los datos que regrela la llamada a la api , la guardamos en el contenedor creado arriba
     })
 
 
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+
+private debouncer: Subject<string> = new Subject<string>()
 
 @Input()
 public placeholdertext: string = ''
@@ -14,13 +18,26 @@ public placeholdertext: string = ''
 @Output()
 public onValue = new EventEmitter<string>
 
+ngOnInit(): void {
+  this.debouncer
+  .pipe(
+    debounceTime(300)
+  )
+  .subscribe( value =>
+  {
+    console.log('valor', value);
+  })
+}
+
 public emitValue ( value: string ):void {
   this.onValue.emit(value)
 }
-
 //*Hemos creado la función emitValue, la cual toma un string como argumento y no retorna ningún valor. Esta función se encarga de asignar el valor recibido al escribir en el input del searchBox (que se pasa como "value"), al output "onValue" utilizando la función emit(). De este modo, prepara los datos para que puedan ser recibidos en otro componente.
-
 //onValue emitValue placeholder
 
+onKeyPress( searchTerm: string )
+{
+  this.debouncer.next( searchTerm )
+}
 
 }
