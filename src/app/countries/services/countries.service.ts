06ec2a1900a572364960cb4b947a0a5a4eef3f6e
fareh150 <37407746+fareh150@users.noreply.github.com>
2024-08-05 11:32:41 +0200

    refactor

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,66 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of} from 'rxjs';
+import { Observable, catchError, delay, map, of} from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
-export class CountriesService {
-
-
+export class CountriesService
+{
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
+  constructor( private httpClient: HttpClient ) { }
 
+  private getCountriesRequest( url: string ): Observable<Country[]>
+  {
+    //?                      recibe un array de tipo Country
+    return this.httpClient.get<Country[]>(url)
+      .pipe
+      (
+        catchError(error => of([])),
+        delay( 2000 )
+      )
+      //pipe ayuda a modificar la respuesta de las peticiones
+      //?pillamos el error  y con el of hacemos que la respuesta se vuelva un array vacio asi modificamos la tabla que mostramos
+  }
 
-
-  constructor(private httpClient: HttpClient) { }
-
-  searchCountryByAlphaCode (code :string): Observable<Country | null> {
+  searchCountryByAlphaCode ( code: string ): Observable<Country | null>
+  {
     //https://restcountries.com/v3.1/alpha/cri
   const url = `${this.apiUrl}/alpha/${code}`
-
   return this.httpClient.get<Country[]>(url)
     .pipe(
       map( countries  => countries.length>0 ? countries[0] : null),
       catchError( () => of (null))
     )
-
   }
 
-
-  searchCapital ( term: string ) : Observable<Country[]>{
-
+  searchCapital ( term: string ) : Observable<Country[]>
+  {
     const url = `${this.apiUrl}/capital/${term}`
-//?                      recibe un array de tipo Country
-    return this.httpClient.get<Country[]>(url)
-      .pipe(
-        catchError(error => of([]))
-      )
-      //pipe ayuda a modificar la respuesta de las peticiones
-      //?pillamos el error  y con el of hacemos que la respuesta se vuelva un array vacio asi modificamos la tabla que mostramos
-
+    return this.getCountriesRequest(url)
   }
 
   searchCountry ( term: string ): Observable<Country[]>  {
     const url = `${this.apiUrl}/name/${term}`
-
-    return this.httpClient.get<Country[]>(url)
-    .pipe(
-      catchError( () => of ([]))
-    )
-
+    return this.getCountriesRequest(url)
   }
 
-  searchRegion ( term: string ) {
+  searchRegion ( term: string )
+  {
   //https://restcountries.com/v3.1/region/{region}
-  const url = `${this.apiUrl}/region/${term}`
-
-  return this.httpClient.get<Country[]>(url)
-    .pipe(
-      catchError( () => of ([]))
-    )
-
-
+    const url = `${this.apiUrl}/region/${term}`
+    return this.getCountriesRequest(url)
   }
-
 }
